dee1a04e56225ae3c49287cce137d08d4c4e6ae5
karita@home <shigekikarita@gmail.com>
2015-10-18 04:15:47 +0900

    update git.sh

--- src/git.sh
+++ src/git.sh
@@ -1,14 +1,22 @@
-gitzatsu() {
-    git add --all
-    git commit -m "雑なコミット"
-    git push origin master
-}
-
 gitup() {
+    if [ $# -ne 2 ]; then
+        cat <<EOF
+Note:
+  add, commit, push
+Usage:
+  gitup "commit messages"
+EOF
+    fi
+
     git add -A
+    git status
+    ask_ok
+
     git commit -m "${1}"
     git push -u origin master
 }
+
+alias gitzatsu='gitup "雑なコミット"'
 
 gitopen(){
   REPO="$(git remote -v | grep fetch | sed 's/origin//' | tr ':' '/' | sed 's/.*git@/http:\/\//' | sed 's/.git *([a-z]*)//')"
