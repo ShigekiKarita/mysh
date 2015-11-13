77063c13b516db6979174e592fa0734bfe58695e
Shigeki Karita <shigekikarita@gmail.com>
2015-11-13 18:12:50 +0900

    add reset-keyboard

--- src/os.sh
+++ src/os.sh
@@ -10,3 +10,4 @@ alias open=xdg-open
 export EDITOR=emacs
 
 alias reset-mouse='sudo modprobe -r psmouse &&  sudo modprobe psmouse'
+alias reset-keyboard='setxkbmap -rules evdev -model jp106 -layout jp'
